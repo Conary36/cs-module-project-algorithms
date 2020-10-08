da25aec0000c9d6dbbd2b7aac2a7d775d01b806e
Conary36 <cbeckford144@gmail.com>
2020-10-07 20:15:56 -0400

    ...here i go...

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -2,18 +2,27 @@
 
 import sys
 
-def making_change(amount, denominations):
-  # Your code here
 
-  pass
+def making_change(amount, denominations):
+    # Your code here
+    #  check if input is small, or large amount.
+    if amount >= 1:
+      denominations = [*args]
+      #  denominations are a list of Integers  chosen by user.
+      #  return the value from dividing the amount by the denominations
+
+
+
+    pass
 
 
 if __name__ == "__main__":
-  # Test our your implementation from the command line
-  # with `python making_change.py [amount]` with different amounts
-  if len(sys.argv) > 1:
-    denominations = [1, 5, 10, 25, 50]
-    amount = int(sys.argv[1])
-    print("There are {ways} ways to make {amount} cents.".format(ways=making_change(amount, denominations), amount=amount))
-  else:
-    print("Usage: making_change.py [amount]")
+    # Test our your implementation from the command line
+    # with `python making_change.py [amount]` with different amounts
+    if len(sys.argv) > 1:
+        denominations = [1, 5, 10, 25, 50]
+        amount = int(sys.argv[1])
+        print("There are {ways} ways to make {amount} cents.".format(ways=making_change(amount, denominations),
+                                                                     amount=amount))
+    else:
+        print("Usage: making_change.py [amount]")
